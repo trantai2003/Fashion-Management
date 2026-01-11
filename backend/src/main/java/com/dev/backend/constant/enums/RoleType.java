735ea5a72fdc90678c311311f2fd52a40cb45cc5
Tai.DevMamNon <102936431+trantai2003@users.noreply.github.com>
2026-01-11 17:04:28 +0700

    feat: enum

--- backend/src/main/java/com/dev/backend/constant/enums/RoleType.java
+++ backend/src/main/java/com/dev/backend/constant/enums/RoleType.java
@@ -1,5 +1,5 @@
 package com.dev.backend.constant.enums;
 
 public enum RoleType {
-    ADMIN, SALE, AGENT, CUSTOMER
+    quan_tri_vien, quan_ly_kho, nhan_vien_kho, nhan_vien_ban_hang, nhan_vien_mua_hang, khach_hang
 }
