72b5e7d141f323b56931e4562c904d0610475a37
Tai.DevMamNon <102936431+trantai2003@users.noreply.github.com>
2026-02-01 12:55:11 +0700

    feat: order details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,6 +22,7 @@ import ResetUserPasswordByAdmin from "@/pages/admin/ResetUserPasswordByAdmin.jsx
 import EditUserRoleByAdmin from "@/pages/admin/EditUserRoleByAdmin.jsx";
 import DashboardByAdmin from "@/pages/admin/DashboardByAdmin.jsx";
 import ColorSizeManagement from "@/pages/admin/ColorSizeManagement.jsx";
+import OrderDetails from "./pages/purchase/OrderDetails";
 export default function App() {
   return (
     <BrowserRouter>
@@ -79,6 +80,9 @@ export default function App() {
           <Route path="/supplier/new" element={<SupplierDetail />} />
           <Route path="/supplier/view/:id" element={<SupplierDetailView />} />
           <Route path="/supplier/:id" element={<SupplierDetail />} />
+
+          {/* Purchase Orders */}
+          <Route path="/purchase-orders/:id" element={<OrderDetails />} />
         </Route>
 
         {/* ========== 404 ========== */}
