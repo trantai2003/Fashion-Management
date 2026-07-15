1d3cd099148e553df00c9ecb2ba956a9c97290c1
Tai.DevMamNon <102936431+trantai2003@users.noreply.github.com>
2026-07-15 23:27:48 +0700

    push

--- backend/src/main/java/com/dev/backend/config/MailConfig.java
+++ backend/src/main/java/com/dev/backend/config/MailConfig.java
@@ -17,7 +17,7 @@ public class MailConfig {
         mailSender.setPort(587);
 
         mailSender.setUsername("trantai17102003@gmail.com");
-        mailSender.setPassword("mkde olop zksj ojzi"); // <- App Password
+        mailSender.setPassword("hcmk enzk ddwr nqmh"); // <- App Password
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
